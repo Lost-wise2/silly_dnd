dc1843056c6efd7f887da0e417a1931a9d347da0
Lost-wise2 <nori.rice0@gmail.com>
2025-10-31 12:02:31 +0100

    added main class for characters and other sub classes for characters

--- dnd_game.py
+++ dnd_game.py
@@ -1,3 +1,29 @@
 #Tehee
 
 print('yaya')
+
+
+class Character():
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def creation(self):
+        print(f'Character {self.name} has joined us, being a {self.type}.')
+
+class Rogue(Character):
+    def __init__(self, name, type, agility):
+        super().__init__(name, type)
+        self.agility = agility
+
+
+class Mage(Character):
+    def __init__(self, name, type, mana):
+        super().__init__(name, type)
+        self.mana = mana
+
+
+class Warrior(Character):
+    def __init__(self, name, type, strength):
+        super().__init__(name, type)
+        self.strength = strength
