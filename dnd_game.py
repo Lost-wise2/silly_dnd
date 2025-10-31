5a976a4b4fe5760de2cb6e502a99851cd94082f4
Lost-wise2 <nori.rice0@gmail.com>
2025-10-31 20:06:28 +0100

    IT WORKS, FIRST DRAFT FOR CHOOSING WHAT TYPE OF CHARACTER AND THEN ADDING NAME, HUZZAHHHHH

--- dnd_game.py
+++ dnd_game.py
@@ -4,24 +4,23 @@ print('yaya')
 
 
 class Character():
-    def __init__(self, name, type):
+    def __init__(self, name):
         self.name = name
-        self.type = type
 
         self.health = 100
         self.ult = 0
         # health and other attributes such as mana and agility are predetermined, 
         # cannot ask user for imput bc they would obvi pick an overpowerd stats
 
-    def creation(self):
-        print(f'Character {self.name} has joined us, being a {self.type}.')
+    #def creation(self):
+        #print(f'Character {self.name} has joined us, being a {self.type}.')
 
     def shield(self):
         print(f'{self.name} used shields')
 
 class Rogue(Character):
-    def __init__(self, name, type, agility):
-        super().__init__(name, type)
+    def __init__(self, name, agility):
+        super().__init__(name)
         self.agility = agility
 
     def add(self): # to test if the ult system works
@@ -39,8 +38,8 @@ class Rogue(Character):
 
 
 class Mage(Character):
-    def __init__(self, name, type, mana):
-        super().__init__(name, type)
+    def __init__(self, name, mana):
+        super().__init__(name)
         self.mana = mana
 
     def L_ATK():
@@ -54,8 +53,8 @@ class Mage(Character):
 
 
 class Warrior(Character):
-    def __init__(self, name, type,  strength):
-        super().__init__(name, type)
+    def __init__(self, name,  strength):
+        super().__init__(name)
         self.strength = strength
     
 
@@ -72,6 +71,36 @@ class Warrior(Character):
 class Arena():
     def __init__(self):
         self.yaya = 0
+
+
+    #def how_many(self):
+    #    global player_count
+    #    how_player_count = int(input('how many?'))
+    #    x = 0
+    #    player_count = []
+    #    while x < how_player_count:
+    #        x += 1
+    #        player_count.append(x)
+
+
+    def character_choice(self, player):
+        global choice_name
+        global type
+        self.player = player
+        print(f'{self.player} select your style:')
+        choice_type = int(input('1. for rogue, 2. for mage and 3. for warrior'))
+        choice_name = input('Insert name for character:')
+        if choice_type == 1:
+            type = Rogue
+        elif choice_type == 2:
+            type = Mage
+        elif choice_type == 3:
+            type = Warrior
+        else:
+            print('oj')
+
+        
+
     def choice(self, person):
         yaya = int(input('0. for shield, 1. for L, 2. for M, 3. for H and 4. for ult'))
         self.yaya = yaya
@@ -88,13 +117,39 @@ class Arena():
 
 
 
-new = Character('Michelle', 'Spectator')
-silly = Rogue('two dolla', 'rogue', 20)
-new.creation()
+new = Character('Michelle')
+silly = Rogue('two dolla', 20)
+#new.creation()
 silly.add()
 silly.add()
 
 place = Arena()
 
-place.choice(silly)
-place.choice(new)
+#place.choice(silly)
+#place.choice(new)
+
+#place.how_many()
+
+#for player in player_count:
+#    place.character_choice(player)
+
+player_1 = 'player_1'
+player_2 = 'player_2'
+
+place.character_choice(player_1)
+player_1 = type(choice_name, 20)
+print(player_1)
+print(silly)
+
+place.character_choice(player_2)
+player_2 = type(choice_name, 10)
+print(player_2)
+
+
+#place.character_choice(player_2)
+
+
+
+
+
+place.choice(player_1)
