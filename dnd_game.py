ac2d9d4bb10e9a9532517d43f6ec0cac88ea7d17
Lost-wise2 <nori.rice0@gmail.com>
2025-10-31 18:10:21 +0100

    added arena? and tested a way to ask for which attack etc :3

--- dnd_game.py
+++ dnd_game.py
@@ -8,24 +8,33 @@ class Character():
         self.name = name
         self.type = type
 
+        self.health = 100
+        self.ult = 0
+        # health and other attributes such as mana and agility are predetermined, 
+        # cannot ask user for imput bc they would obvi pick an overpowerd stats
+
     def creation(self):
         print(f'Character {self.name} has joined us, being a {self.type}.')
 
-    def shield():
-        pass
+    def shield(self):
+        print(f'{self.name} used shields')
 
 class Rogue(Character):
     def __init__(self, name, type, agility):
         super().__init__(name, type)
         self.agility = agility
 
-    def L_ATK():
-        pass
+    def add(self): # to test if the ult system works
+        self.ult += 10
+        print(f'ult points now are {self.ult}')
+
+    def L_ATK(self):
+        print(f'{self.name} used a light attack')
     def M_ATK():
         pass
     def H_ATK():
         pass
-    def special():
+    def special(self):
         pass
 
 
@@ -45,7 +54,7 @@ class Mage(Character):
 
 
 class Warrior(Character):
-    def __init__(self, name, type, strength):
+    def __init__(self, name, type,  strength):
         super().__init__(name, type)
         self.strength = strength
     
@@ -61,4 +70,31 @@ class Warrior(Character):
 
 
 class Arena():
-    pass
+    def __init__(self):
+        self.yaya = 0
+    def choice(self, person):
+        yaya = int(input('0. for shield, 1. for L, 2. for M, 3. for H and 4. for ult'))
+        self.yaya = yaya
+        if yaya == 0:
+            person.shield()
+        if yaya == 1:
+            person.L_ATK()
+        if yaya == 2:
+            pass
+        if yaya == 3:
+            pass
+        if yaya == 4:
+            pass
+
+
+
+new = Character('Michelle', 'Spectator')
+silly = Rogue('two dolla', 'rogue', 20)
+new.creation()
+silly.add()
+silly.add()
+
+place = Arena()
+
+place.choice(silly)
+place.choice(new)
