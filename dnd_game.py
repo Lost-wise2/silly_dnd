1d4382db1d0ded83dd226cfb5538f190fd5ac4fa
Lost-wise2 <nori.rice0@gmail.com>
2025-10-31 16:23:44 +0100

    added some placeholder functions

--- dnd_game.py
+++ dnd_game.py
@@ -11,10 +11,22 @@ class Character():
     def creation(self):
         print(f'Character {self.name} has joined us, being a {self.type}.')
 
+    def shield():
+        pass
+
 class Rogue(Character):
     def __init__(self, name, type, agility):
         super().__init__(name, type)
         self.agility = agility
+
+    def L_ATK():
+        pass
+    def M_ATK():
+        pass
+    def H_ATK():
+        pass
+    def special():
+        pass
 
 
 class Mage(Character):
@@ -22,8 +34,31 @@ class Mage(Character):
         super().__init__(name, type)
         self.mana = mana
 
+    def L_ATK():
+        pass
+    def M_ATK():
+        pass
+    def H_ATK():
+        pass
+    def special():
+        pass
+
 
 class Warrior(Character):
     def __init__(self, name, type, strength):
         super().__init__(name, type)
         self.strength = strength
+    
+
+    def L_ATK():
+        pass
+    def M_ATK():
+        pass
+    def H_ATK():
+        pass
+    def special():
+        pass
+
+
+class Arena():
+    pass
